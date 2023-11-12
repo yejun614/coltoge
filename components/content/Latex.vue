08bd38050e14608c7f43a4cb9d4524e1140d3ba0
YeJun, Jung <yejun614@naver.com>
2023-11-13 06:37:34 +0900

    Feat: latex 문법

---
+++ components/content/Latex.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { LaTeXJSComponent } from "latex.js";
+
+onMounted(() => {
+    customElements.define("latex-js", LaTeXJSComponent);
+});
+</script>
+
+<template>
+    <latex-js>
+        <ContentSlot :use="$slots.default" unwrap="p" />
+    </latex-js>
+</template>
